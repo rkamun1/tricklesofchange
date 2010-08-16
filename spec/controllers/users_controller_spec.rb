edd0ca96c4aaca126b2a9e22dac1590a3ff1aea4
touchInspiration <touchinspiration@freewilly-ubuntu.(none)>
2010-08-16 16:00:37 -0400

    User signup complete

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,11 +5,11 @@ describe UsersController do
 	
   describe "GET 'new'" do
     it "should be successful" do
-      get 'new'
+      get :new
       response.should be_success
     end
 		it "should have the right title" do
-			get 'new'
+			get :new
 			response.should have_selector("title", :content =>"Sign Up")
 		end	
   end
@@ -22,6 +22,11 @@ describe UsersController do
     it "should be successful" do
       get :show, :id => @user
       response.should be_success
+    end
+    
+    it "should have the right title" do
+      get :new
+      response.should have_selector("title", :content => "Sign Up")
     end
     
     it "should find the right user" do
@@ -43,4 +48,57 @@ describe UsersController do
       response.should have_selector("h1>img", :class => "gravatar")
     end
   end
+  
+  describe "POST 'create'" do
+    describe "failure" do
+      
+      before(:each) do
+        @attr = {:name=>"", :email=>"", :password=>"", 
+                 :password_confirmation=>"" }
+      end
+      
+      it "should not create a user" do
+        lambda do
+          post :create, :user => @attr
+        end.should_not change(User, :count)
+      end
+      
+      it "should have the right title" do
+        post :create, :user => @attr
+        response.should have_selector("title", :content => "Sign Up")
+      end
+      
+      it "should render the 'new' page" do
+        post :create, :user => @attr
+        response.should render_template('new')
+      end
+    end
+    
+  describe "success" do
+
+      before(:each) do
+        @attr = { :name => "New User", 
+                  :email => "user@example.com",
+                  :password => "foobar", 
+                  :password_confirmation => "foobar" }
+      end
+
+      it "should create a user" do
+        lambda do
+          post :create, :user => @attr
+        end.should change(User, :count).by(1)
+      end
+
+      it "should redirect to the user show page" do
+        post :create, :user => @attr
+        response.should redirect_to(user_path(assigns(:user)))
+      end 
+      
+      it "should have a welcome message" do
+        post :create, :user => @attr
+        flash[:success].should =~ /welcome to the chirp/i
+      end   
+    end
+  end
 end
+
