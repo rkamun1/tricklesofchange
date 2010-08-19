6fe1336deb43c2231230161c88e279c0bc728ab2
Sir Bertly <sirbertly@touchinspiration.com>
2010-08-19 14:18:28 -0400

    User's in place

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,6 +18,20 @@ set :use_sudo, false
 server domain, :app, :web
 role :db, domain, :primary => true
 
+namespace :bundler do
+  task :create_symlink, :roles => :app do
+    shared_dir = File.join(shared_path, 'bundle')
+    release_dir = File.join(current_release, '.bundle')
+    run("mkdir -p #{shared_dir} && ln -s #{shared_dir} #{release_dir}")
+  end
+ 
+  task :bundle_new_release, :roles => :app do
+    bundler.create_symlink
+    run "cd #{release_path} && bundle install --without test --without development"
+  end
+end
+ 
+after 'deploy:update_code', 'bundler:bundle_new_release'
 namespace :deploy do
   task :restart do
     run "touch #{current_path}/tmp/restart.txt"
