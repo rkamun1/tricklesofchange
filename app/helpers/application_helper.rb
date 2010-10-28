9b4f0cd4f91c0c79fb63a3bcb869f348895147fa
Sir Bertly <sirbertly@freewilly-ubuntu.(none)>
2010-10-28 19:51:33 -0400

    graphics

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   
  # Return a title on a per-page basis.
   def title
-    base_title = "Trickles of Change"
+    base_title = "Trickles of Change - Your tool to microsaving!"
     if @title.nil?
       base_title
     else
