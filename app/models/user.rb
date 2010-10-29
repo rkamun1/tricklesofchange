16634375c1acd846ccf528bc3b13f890384a65b9
Sir Bertly <sirbertly@freewilly-ubuntu.(none)>
2010-10-29 11:57:13 -0400

    testing crinjob

--- app/models/user.rb
+++ app/models/user.rb
@@ -122,7 +122,7 @@ class User < ActiveRecord::Base
       #check timezone.
       Time.zone = user.timezone
       puts Time.zone.now
-      if Time.zone.now.hour == Time.zone.now.midnight.hour #this makes it run at the midnight hour.
+      if Time.zone.now.hour == Time.zone.now.hour #this makes it run at the midnight hour.
       	puts "in midnight"
         #Default case: update the deets assuming that the user spent nothing that day
 				distributed_amount = 0
