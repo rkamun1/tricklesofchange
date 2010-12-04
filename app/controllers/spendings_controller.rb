baf565f38a28cbbf9b4532353b070236f8f657dc
Sir Bertly <sirbertly@freewilly-ubuntu.(none)>
2010-12-04 16:51:54 -0500

    Finally fixed the spending date check

--- app/controllers/spendings_controller.rb
+++ app/controllers/spendings_controller.rb
@@ -14,6 +14,7 @@ class SpendingsController < ApplicationController
   def create
     @spending = current_user.spendings.build(params[:spending])
     @user = @spending.user
+    @accounts = @user.accounts
     if @spending.save
       flash[:success] = "Your spending has been saved!"
       respond_to do |format|  
@@ -41,6 +42,7 @@ class SpendingsController < ApplicationController
   def update
     @spending = Spending.find(params[:id])
     @user = @spending.user
+    @accounts = @user.accounts
     if @spending.update_attributes(params[:spending])
       flash[:success] = "Spending updated."
       respond_to do |format|  
@@ -59,6 +61,7 @@ class SpendingsController < ApplicationController
   def destroy
     @spending = Spending.find(params[:id])
     @user = @spending.user
+    @accounts = @user.accounts
     @spending.destroy
     respond_to do |format|  
        format.html{redirect_to root_path}
